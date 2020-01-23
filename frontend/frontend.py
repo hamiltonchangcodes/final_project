ea5af47ae89b6705ea031b03c3f71d8e14f56a00
Hamilton Chang <hammychang@gmail.com>
2020-01-23 12:58:30 -0500

    snapshots added to front end

--- frontend/frontend.py
+++ frontend/frontend.py
@@ -15,26 +15,26 @@ import numpy as np
 import pandas as pd
 
 
-# In[ ]:
+# In[2]:
 
 
 def load_data(data):
     if data == 'Manhattan':
-        df = pd.read_csv('displaycardManhattan.csv')
-       
+        df = pd.read_csv('displaycardManhattan.csv', encoding='utf-8')
+        sn = pd.read_csv('Manhattan_snapshot.csv', encoding='utf-8')
     elif data == 'Brooklyn':
-        df = pd.read_csv('displaycardBrooklyn.csv')
+        df = pd.read_csv('displaycardBrooklyn.csv', encoding='utf-8')
         
     elif data == 'Queens':
-        df = pd.read_csv('displaycardQueens.csv')
+        df = pd.read_csv('displaycardQueens.csv', encoding='utf-8')
       
     elif data == 'Staten Island':
-        df = pd.read_csv('displaycardStatenIsland.csv')
+        df = pd.read_csv('displaycardStatenIsland.csv', encoding='utf-8')
       
     elif data == 'The Bronx':
-        df = pd.read_csv('displaycardBronx.csv')
+        df = pd.read_csv('displaycardBronx.csv', encoding='utf-8')
    
-    return df
+    return df, sn
 
 
 # In[9]:
@@ -59,7 +59,7 @@ st.image(image, caption='Yelp!', use_column_width=False, format='PNG')
 # In[12]:
 
 
-st.markdown('Welcome to the Yelp Report Card.  For this project, I collected approximately 115,000 reviews from 896 restaurants in New York City.  I then processed them for Sentiment Analysis to determine if the review was positive or negative.  My next step was to use Latent Dirichlet Alloction or LDA on the approximately 3.7 million words in the reviews to determine the topic of the review, separated as Food, or Service.  Lastly, I combined my two analysis to determine if the reveiwer liked the food/service, or did not like the food/service and produced a final score as a percentage of responses.')
+st.markdown('Welcome to the Yelp Report Card.  For this project, I collected approximately 460,000 reviews from 5000 restaurants in New York City.  I then processed them for Sentiment Analysis to determine if the review was positive or negative.  My next step was to use Latent Dirichlet Alloction or LDA on the approximately 15 million words in the reviews to determine the topic of the review, separated as Food, or Service.  Lastly, I combined my two analysis to determine if the reveiwer liked the food/service, or did not like the food/service and produced a final score as a percentage of responses.')
 
 
 # In[13]:
@@ -113,19 +113,27 @@ boro = st.sidebar.selectbox('Choose a Borough', boros)
 # In[ ]:
 
 
-df = load_data(boro)
+df, sn = load_data(boro)
+
+
+# In[ ]:
+
+
+
 
 
 # In[ ]:
 
 
 df = df.set_index('restaurant')
+sn = sn.set_index('restaurant')
 
 
 # In[ ]:
 
 
 df = df.drop(columns = 'Unnamed: 0')
+sn = sn.drop(columns = 'Unnamed: 0')
 
 
 # In[ ]:
@@ -154,10 +162,58 @@ st.balloons()
 st.write(df.loc[option])
 
 
+# In[ ]:
+
+
+
+
+
 # In[9]:
 
 
 st.bar_chart(df.loc[option], height=40)
+
+
+# In[ ]:
+
+
+st.write('The below is a snapshot of some Positive and Negative Reviews')
+
+
+# In[ ]:
+
+
+st.write(sn.loc[option]['Positive Review 1'])
+
+
+# In[ ]:
+
+
+st.write(sn.loc[option]['Positive Review 2'])
+
+
+# In[ ]:
+
+
+st.write(sn.loc[option]['Positive Review 3'])
+
+
+# In[ ]:
+
+
+st.write(sn.loc[option]['Negative Review 1'])
+
+
+# In[ ]:
+
+
+st.write(sn.loc[option]['Negative Review 2'])
+
+
+# In[ ]:
+
+
+st.write(sn.loc[option]['Negative Review 3'])
 
 
 # In[ ]:
